b816630d897f09083835f32fa83d734776eb57f4
Floriane Didier <florianedidier@gmail.com>
2019-02-27 15:07:41 -0300

    airbnb model done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cobertura with view of the beach',
+  address: 'Copacabana, Rio de Janeiro',
+  description: 'Amazing penthouse with 3 bedrooms in Copa',
+  price_per_night: 60,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Studio Montmartre',
+  address: 'rue des Coquelicots, 75018 Paris',
+  description: 'Charming one bedrom studio in Paris, just next to the Sacre Coeur',
+  price_per_night: 45,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: '2 bedroom in Los Angeles',
+  address: 'Santa Monica',
+  description: 'Very chilled flat right in the middle of Sant Monica beach',
+  price_per_night: 55,
+  number_of_guests: 4
+)
+
